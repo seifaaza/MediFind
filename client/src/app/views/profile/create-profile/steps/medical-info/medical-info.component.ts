2934bcecf2e02e9cf0c739fdc4e70e411d1f256e
seif <seifeddine.aaza@gmail.com>
2024-12-12 02:49:53 +0100

    update

--- client/src/app/views/profile/create-profile/steps/medical-info/medical-info.component.ts
+++ client/src/app/views/profile/create-profile/steps/medical-info/medical-info.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { BackComponent } from '../../../../../core/components/buttons/back/back.component';
+import { environment } from '../../../../../../environments/environment.prod';
+import { AuthService } from '../../../../../core/services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-medical-info',
   standalone: true,
-  imports: [BackComponent],
+  imports: [BackComponent, CommonModule, NzButtonModule, NzIconModule],
   templateUrl: './medical-info.component.html',
-  styleUrl: './medical-info.component.css'
+  styleUrl: './medical-info.component.css',
 })
 export class MedicalInfoComponent {
+  isAuthenticated = false;
+  apiUrl = environment.API_URL;
+  loading = false;
+  errorMessage = '';
 
+  constructor(private authService: AuthService) {}
 }
--- client/src/app/views/profile/create-profile/steps/personal-info/personal-info.component.ts
+++ client/src/app/views/profile/create-profile/steps/personal-info/personal-info.component.ts
@@ -9,6 +9,9 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { AuthService } from '../../../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-personal-info',
@@ -24,12 +27,17 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
     NzAlertModule,
     NzSelectModule,
     FormsModule,
+    NzInputNumberModule,
+    NzDatePickerModule,
   ],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.css',
 })
 export class PersonalInfoComponent {
+  isAuthenticated = false;
   apiUrl = environment.API_URL;
   loading = false;
   errorMessage = '';
+
+  constructor(private authService: AuthService) {}
 }
